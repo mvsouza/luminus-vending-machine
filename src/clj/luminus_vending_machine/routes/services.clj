fc7a085502adca460eae8ef4341916c53eaab839
Matheus Souza <mvs@mvsouza.com>
2018-11-05 19:59:24 -0200

    Post method to insert coins on the machine.

--- src/clj/luminus_vending_machine/routes/services.clj
+++ src/clj/luminus_vending_machine/routes/services.clj
@@ -3,42 +3,23 @@
             [compojure.api.sweet :refer :all]
             [schema.core :as s]))
 
+(def coins-value {:nicle 1/20 :quarter 1/4 :dime 1/10 :half_d 1/2 :dolar 1})
+(def amount-inserted-ref (ref 0.0M))
+
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
              :data {:info {:version "1.0.0"
                            :title "Sample API"
                            :description "Sample Services"}}}}
-  
+
   (context "/api" []
-    :tags ["thingie"]
-    
-    (GET "/plus" []
-      :return       Long
-      :query-params [x :- Long, {y :- Long 1}]
-      :summary      "x+y with query-parameters. y defaults to 1."
-      (ok (+ x y)))
-
-    (POST "/minus" []
-      :return      Long
-      :body-params [x :- Long, y :- Long]
-      :summary     "x-y with body-parameters."
-      (ok (- x y)))
-
-    (GET "/times/:x/:y" []
-      :return      Long
-      :path-params [x :- Long, y :- Long]
-      :summary     "x*y with path-parameters"
-      (ok (* x y)))
-
-    (POST "/divide" []
-      :return      Double
-      :form-params [x :- Long, y :- Long]
-      :summary     "x/y with form-parameters"
-      (ok (/ x y)))
-
-    (GET "/power" []
-      :return      Long
-      :header-params [x :- Long, y :- Long]
-      :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))))
+    :tags ["Vending machine"]
+    (POST "/insert" []
+      :return       Double
+      :body-params [coin :- String]
+      :summary      "coin on body is inserted to the machine."
+      (dosync
+        (alter amount-inserted-ref +
+                     ((keyword coin) coins-value))
+              (ok  @amount-inserted-ref)))))
